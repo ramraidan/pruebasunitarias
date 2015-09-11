89002d7bf248d34d5d498784ef8696a2da60a436
ramraidan <dr.raigoza@gmail.com>
2015-09-11 10:55:20 -0500

    Agregue la prueba para la suma de la calculadora

---
+++ TestCalculadora.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+class TestCalculadora(unittest.TestCase):
+
+	def test_suma_de_2_mas_2(self):
+		calc = Calculadora()
+		resultado = calc.suma(2,2)
+
+		self.assertEqual()
+
+		return resultado
