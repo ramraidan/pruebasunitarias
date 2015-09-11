d2024fc44ab8e9dfe88736a82c6fd046e1e317a2
ramraidan <dr.raigoza@gmail.com>
2015-09-11 11:16:54 -0500

    Agregué mas pruebas y se refactorizo

--- TestCalculadora.py
+++ TestCalculadora.py
@@ -3,10 +3,28 @@ import unittest
 
 class TestCalculadora(unittest.TestCase):
 
+	def setUp(self):
+		self.calc = Calculadora()
+
 	def test_suma_de_2_mas_2(self):
-		calc = Calculadora()
-		resultado = calc.suma(2,2)
+		resultado = self.calc.suma(2,2)
 
-		self.assertEqual()
+		self.assertEqual(4,resultado)
 
-		return resultado
+	def test_suma_de_3_mas_3(self):
+
+		resultado = self.calc.suma(3,3)
+		self.assertEqual(6,resultado)
+
+	def test_suma_de_5_mas_5(self):
+	   	resultado = self.calc.suma(5,5)
+		self.assertEqual(10,resultado)
+
+class Calculadora():
+
+	def suma(self,num1,num2):
+		return num1+num2
+		
+
+if __name__ == '__main__':
+	unittest.main()
